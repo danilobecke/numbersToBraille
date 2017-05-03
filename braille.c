339f41e8ea2e6588f2001f46da7bcceafdc02b20
danilobecke <danilobecke@MacBook-Pro-de-Danilo-2.local>
2017-05-03 02:04:52 -0300

    some fixes.

--- braille.c
+++ braille.c
@@ -76,42 +76,25 @@ int isFromFirstSerie(int* array) {
 }
 
 int isFromSecondSerie(int *array){
-        int size = sizeof(array)/sizeof(int);
-        
-        
-        if(size!=6) {
+    
+    if(sizeOfArray(array) != 6) {
         return 0;
-
-        }
-
-       
-       if(array[3]==1 && array[6]==0){
-       
-        return 1;
-       
-       } 
-       
-       else 
-           
-           return 0;
-       
-       
     }
+    
+    if(array[3]==1 && array[6]==0){
+        return 1;
+    } else
+        return 0;
+}
 
 int isFromThirdSerie(int *array){
-
-    if(size!=6) {
-     return 0;
-     
-    }
     
-    
-    if(array[3]==1 & array[6]=1) {
-    
-     return 1;
-    }
-    
-    else
+    if(sizeOfArray(array) != 6) {
         return 0;
-
+    }
+    
+    if(array[3] == 1 && array[6] == 1) {
+        return 1;
+    } else
+        return 0;
 }
