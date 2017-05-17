c60e53282d95bd63d52cd45d8a1fe745b73fe24a
Danilo Becke <danilo.becke@eldorado.org.br>
2017-05-17 18:55:39 -0300

    changes in tests output

--- UnitTests/main.c
+++ UnitTests/main.c
@@ -56,17 +56,16 @@
 // MAIN
     int main(int argc, char **argv) {
         char *result = all_tests();
-        if (result != 0) {
-            printf("%s\n", result);
-        }
-        else {
-            printf("ALL TESTS PASSED\n");
-        }
-        printf("Tests run: %d\n", tests_run);
-        
+
         printAlphabet();
         printNumbers();
         printf("----------\n");
+        
+        if (result != 0)
+            printf("%s\n", result);
+        else
+            printf("ALL TESTS PASSED\n");
+        printf("Tests run: %d\n", tests_run);
         
         return result != 0;
     }
